e4c71cfd2922e46bf5b359a612d9eedc4ef9a1cd
VeranikaKaraliova <nika-11@mail.ru>
2020-05-07 20:35:54 +0300

    Adding a new solution for the first item

--- 04.05.2020/homework04.05.2020.py
+++ 04.05.2020/homework04.05.2020.py
@@ -1,8 +1,14 @@
 a = int(input())
 b = int(input())
 c = int(input())
+# Вариант решения с if
 if a != 0 and b != 0 and c != 0:
     print('Нет нулевых значений!!!')
+# Вариант решения через словарь
+s = {'value1' : a, 'value2' : b, 'value3' : c}
+res = not any(s.values())
+print('Введены все нули? : ' + str(res))
+
 if a == 0 and b == 0 and c == 0:
     print('Введены все нули!')
 elif a == 0:
